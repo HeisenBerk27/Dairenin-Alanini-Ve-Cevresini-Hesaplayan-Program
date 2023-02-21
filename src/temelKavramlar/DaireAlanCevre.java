07282aa1318d038f53967842313fd4323771e20e
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-21 19:37:36 +0300

    Add files via upload

---
+++ src/temelKavramlar/DaireAlanCevre.java
@@ -0,0 +1,24 @@
+package temelKavramlar;
+
+import java.util.Scanner;
+
+public class DaireAlanCevre {
+	
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final double PI = 3.14;
+
+        System.out.print("Daire diliminin yarıçapını giriniz: ");
+        double r = scanner.nextDouble();
+
+        System.out.print("Daire diliminin merkez açısının ölçüsünü giriniz: ");
+        double alfa = scanner.nextDouble();
+
+        double alan = (PI * (r * r) * alfa) / 360.0;
+
+        System.out.println("Daire diliminin alanı: " + alan);
+    }
+}
+
+
